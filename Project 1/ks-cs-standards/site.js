b9ab3ba636c210503189927256aaa098ad229160
Mike Ruckert <mwruckert@gmail.com>
2020-06-14 18:26:37 -0500

    Grids are fun!

--- Project 1/ks-cs-standards/site.js
+++ Project 1/ks-cs-standards/site.js
@@ -6,6 +6,9 @@
 
 
 document.body.onload = addElement;
+const container = document.getElementById("container");
+
+
 
 function addElement () {
     
@@ -14,24 +17,42 @@ function addElement () {
         var standardDiv = document.createElement("div");
         standardDiv.className = "stdItem";
                 
+        /*var iDiv = document.createElement('div');
+        iDiv.className = "column one";
         var i = document.createElement('p');
-        i.textContent= element.identifier;
+        i.innerHTML= element.identifier;
+        iDiv.appendChild(i);
+        standardDiv.appendChild(iDiv);*/
+
+        var iDiv = document.createElement('div');
+        iDiv.className = "column one"
+        var i = document.createElement('p');
+        i.innerHTML = element.identifier;
+        iDiv.appendChild(i);
+        standardDiv.appendChild(iDiv);
+
+
+        var sDiv = document.createElement('div');
+        sDiv.className = "column two"
 
         var s = document.createElement('p');
-        s.textContent = element.statement;
+        s.innerHTML = element.statement;
+        sDiv.appendChild(s);
 
         var d = document.createElement('p');
-        d.className = "description";
-        d.textContent = element.description;
+        d.innerHTML = element.description;
+        sDiv.appendChild(d);
+
+
 
         var sc = document.createElement('p');
         sc.textContent = element.subconcept;
+        sc.className = "column three";
 
         var p = document.createElement('p');
         p.textContent = element.practices
+        p.className = "column four"
 
-        standardDiv.appendChild(i);
-        standardDiv.appendChild(s);
         standardDiv.appendChild(d);
         standardDiv.appendChild(sc);
         standardDiv.appendChild(p);
